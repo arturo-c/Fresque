3f9b0629ff16ecb74184be0432d3b349db9dc091
Arturo Contreras <arturo@allplayers.com>
2013-04-08 17:23:04 -0500

    Change autoload dir.

--- lib/init.php
+++ lib/init.php
@@ -18,12 +18,6 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-if (file_exists(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'autoload.php')) {
-    require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'autoload.php';
-} else {
-    require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-}
-require __DIR__ . DIRECTORY_SEPARATOR . 'Fresque.php';
-
+require __DIR__ . '/../../../../vendor/autoload.php';
 $fresque = new Fresque\Fresque();
 
